4ac5f7552c6376403171257952813f4ec1329594
AlexPerezDeveloper <alexperez.developer@gmail.com>
2017-08-15 18:38:04 +0200

    Basic Google Auth with React+Firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase';
 import './App.css';
 
 class App extends Component {
+ 
+constructor(){
+    super();
+
+      this.state = {
+        user:null
+      };
+
+
+ this.handleAuth = this.handleAuth.bind(this);
+ this.handleLogout = this.handleLogout.bind(this);
+
+} 
+
+ componentWillMount = () => {
+   firebase.auth().onAuthStateChanged(user =>{
+
+      this.setState({ user:user})
+
+   })
+ }
+ 
+
+
+  handleAuth(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesion`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+
+}
+
+  handleLogout(){
+    firebase.auth().signOut()
+    .then(result => console.log(`${result.user.email} ha salido`))
+    .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+renderLoginButton(){
+  //Si está logueado
+    if(this.state.user != null){
+
+        return(
+          <div className="App-info">
+          <img src={this.state.user.photoURL} alt={this.state.user.displayName}/>
+          <h1>
+            Hola {this.state.user.displayName} 
+            <br/>
+            Tu Email es {this.state.user.email}
+          </h1>
+                 <button onClick={this.handleLogout}>Salir</button>
+
+          </div>
+
+        );
+
+    }else{
+      return(
+       <button onClick={this.handleAuth}>Login con Google</button>
+      );
+    }
+  //Si no está logueado
+}
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>CloneGram</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{ this.renderLoginButton() }</p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBVI6zNyG9H2pq2OMRaJrV5Clf7zTpGvI0",
+    authDomain: "clonegram-1c26c.firebaseapp.com",
+    databaseURL: "https://clonegram-1c26c.firebaseio.com",
+    projectId: "clonegram-1c26c",
+    storageBucket: "",
+    messagingSenderId: "56608186317"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
